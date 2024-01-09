f7ba1c07d2641dfdbabcaaf4c79cb681f136c9fe
Yll Sheremeti <116513114+YllSheremeti@users.noreply.github.com>
2024-01-09 22:12:46 +0100

    other changes

--- JS/login.js
+++ JS/login.js
@@ -1,3 +1,99 @@
+class FormHandler {
+  //construktor per manip me array
+    constructor() {
+      this.numbers = [1, 2, 3, 4, 5];
+      this.initializeEvents();
+    }
+  // funks per submit te loginform
+    initializeEvents() {
+      $(document).ready(() => {
+        $('#loginForm').submit((event) => {
+          event.preventDefault(); 
+          this.handleLoginForm();
+        });
+      });
+    }
+  
+    handleLoginForm() {
+      // Get the value of the username input
+      let usernameValue = $('input[name="username"]').val();
+      console.log('Username:', usernameValue);
+  
+      // Set a new value to the password input
+      $('input[name="password"]').val('newPassword');
+  
+      // Add a class to the username input
+      $('input[name="username"]').addClass('new-class');
+  
+      // Remove a class from the password input
+      $('input[name="password"]').removeClass('form-control');
+  
+      window.location.href = '../Pages/home.html'; 
+    }
+  
+
+
+
+    manipulateNumbers() {
+      //perdorimi i map per 2fishin e nr ne array
+      let doubledNumbers = this.numbers.map((number) => {
+        return number * 2;
+      });
+      console.log('Doubled Numbers:', doubledNumbers);
+  
+      // Perdorimi i reduce() per shum
+      let sum = this.numbers.reduce((accumulator, currentValue) => {
+        return accumulator + currentValue;
+      }, 0);
+      console.log('Sum:', sum);
+  
+      //perdorimi i filter() per nr tek
+      let oddNumbers = this.numbers.filter((number) => {
+        return number % 2 !== 0;
+      });
+      console.log('Odd Numbers:', oddNumbers);
+    }
+  
+    toExponentialNumbers() {
+      //perdorimi i map()
+      let exponentialNumbers = this.numbers.map((number) => {
+        return number.toExponential();
+      });
+      console.log('Exponential Numbers:', exponentialNumbers);
+      return exponentialNumbers;
+    }
+  }
+  
+  // instanc e klases formHandler
+  const formHandler = new FormHandler();
+  
+ //thirrja e funks manipulateNumbers()
+  formHandler.manipulateNumbers();
+  
+  //thirrja e funks toExponentialNumbers()
+  formHandler.toExponentialNumbers();
+  
+
+
+
+
+  //drag and drop
+
+  function allowDrop(ev) {
+    ev.preventDefault();
+  }
+  
+  function drag(ev) {
+    ev.dataTransfer.setData("text", ev.target.id);
+  }
+  
+  function drop(ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("text");
+    ev.target.appendChild(document.getElementById(data));
+  }
+
+  
 //show and hide
 
 function toggleVisibility() {
@@ -9,6 +105,7 @@ function toggleVisibility() {
     element.style.display = "none"; 
   }
 }
+
 
 //validimi i checkbox
 function validateForm() {
