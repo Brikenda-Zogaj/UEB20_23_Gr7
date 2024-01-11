acd9c574a972c53cb709637178713d2a025db177
gr3sa-h1 <154631164+gr3sa-h1@users.noreply.github.com>
2024-01-11 02:18:07 +0100

    Updated contact.js

--- JS/contact.js
+++ JS/contact.js
@@ -101,3 +101,12 @@ function tellTime(){
      navbar.classList.remove("sticky");
    }
  }
+//faq js
+function toggleAnswer(id) {
+  var answer = document.getElementById(id);
+  if (answer.style.display === 'none') {
+      answer.style.display = 'block';
+  } else {
+      answer.style.display = 'none';
+  }
+}
