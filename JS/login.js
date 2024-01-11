bb7a5ff9215384d7d6305d68a03d4d2806e4228e
ritaberishaa <76747778+ritaberishaa@users.noreply.github.com>
2024-01-11 14:27:50 +0100

    login navbar changes

--- JS/login.js
+++ JS/login.js
@@ -201,3 +201,14 @@ function validateForm() {
 
   return true; 
 }
+ // Sticky Navbar
+ window.onscroll = function() { scrollFunction() };
+ function scrollFunction() {
+   var navbar = document.getElementById("navbar");
+   var sticky = navbar.offsetTop;
+   if (window.pageYOffset >= sticky) {
+     navbar.classList.add("sticky");
+   } else {
+     navbar.classList.remove("sticky");
+   }
+ }
